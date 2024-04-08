5c4977a0a4ed71ba8122dabdabdc6d184239ce0c
akostian <akostian@student.s19.be>
2024-04-08 10:19:50 +0200

    ft_isdigit

---
+++ ft_isdigit.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 10:00:32 by akostian          #+#    #+#             */
+/*   Updated: 2024/04/08 10:03:21 by akostian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/* #include <stdio.h>
+int	main(void)
+{
+	printf("%d\n", ft_isdigit('a'));
+	printf("%d\n", ft_isdigit('b'));
+	printf("%d\n", ft_isdigit('6'));
+	printf("%d\n", ft_isdigit('*'));
+	printf("%d\n", ft_isdigit('0'));
+} */
