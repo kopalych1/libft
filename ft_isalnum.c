42252d76b6453e3f45cd336ad0f6693bf3a36b2c
akostian <akostian@student.s19.be>
2024-04-08 10:21:04 +0200

    ft_isalnum

---
+++ ft_isalnum.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 10:04:40 by akostian          #+#    #+#             */
+/*   Updated: 2024/04/08 10:04:40 by akostian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isalnum(char c)
+{
+	return ((c >= '0' && c <= '9')
+		|| (c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z'));
+}
+
+/* #include <stdio.h>
+int	main(void)
+{
+	printf("%d\n", ft_isalnum('a'));
+	printf("%d\n", ft_isalnum('b'));
+	printf("%d\n", ft_isalnum('6'));
+	printf("%d\n", ft_isalnum('*'));
+	printf("%d\n", ft_isalnum('&'));
+} */
