efcdfca93453f07890625209720037328076c7b0
akostian <akostian@student.s19.be>
2024-04-08 10:15:50 +0200

    ft_isalpha

---
+++ ft_isalpha.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 09:55:16 by akostian          #+#    #+#             */
+/*   Updated: 2024/04/08 09:57:10 by akostian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isalpha(char c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+/* #include <stdio.h>
+int	main(void)
+{
+	printf("%d\n", ft_isalpha('a'));
+	printf("%d\n", ft_isalpha('b'));
+	printf("%d\n", ft_isalpha('6'));
+	printf("%d\n", ft_isalpha('*'));
+	printf("%d\n", ft_isalpha('&'));
+} */
