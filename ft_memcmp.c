2f8aa9d8b927321d6c76fd7db29dad66d947f190
akostian <akostian@student.s19.be>
2024-04-09 11:24:31 +0200

    ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/09 11:15:23 by akostian          #+#    #+#             */
+/*   Updated: 2024/04/09 11:23:16 by akostian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_memcmp(const void *s1, const void *s2, unsigned long int n)
+{
+	char	*s1_cpy;
+	char	*s2_cpy;
+
+	s1_cpy = (char *)s1;
+	s2_cpy = (char *)s2;
+	while (n-- > 0)
+	{
+		if (*s1_cpy != *s2_cpy)
+			return (*s1_cpy - *s2_cpy);
+		s1_cpy++;
+		s2_cpy++;
+	}
+	return (0);
+}
+/* 
+#include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	char	str1[77] = "jshfisheiufhsieufh";
+	char	str2[77] = "jshfisheiufhsieufh";
+
+	printf("%d\n", ft_memcmp(str1, str2, 60));
+	printf("%d\n", memcmp(str1, str2, 60));
+} */
