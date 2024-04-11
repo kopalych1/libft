7612fb89552e85af6124e30d458e95e36cdeeb40
Aleksieiev Kostiantyn <akostian@student.s19.b>
2024-04-11 14:55:31 +0200

    libft.h summing up the functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:44:19 by akostian          #+#    #+#             */
-/*   Updated: 2024/04/10 19:41:32 by akostian         ###   ########.fr       */
+/*   Updated: 2024/04/11 14:54:28 by akostian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <stdlib.h>
 
+/* IS */
+
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
@@ -22,9 +24,33 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_isspace(int c);
 
-size_t	ft_strlen(const char *s);
+/* MEM */
 
+char	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+
+/* STR */
+
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *src);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(char *s1, char *s2, size_t n);
+char	*ft_strnstr(char *big, char *little, size_t len);
+char	*ft_strrchr(char *s, int c);
+
+/* TO */
+int		ft_atoi(char *addr);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+/*  */
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
