0d20740a7e3150db3db84acc00e105d1b5865d53
akostian <akostian@student.s19.be>
2024-04-15 13:12:14 +0200

    ft_lstadd_front

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:44:19 by akostian          #+#    #+#             */
-/*   Updated: 2024/04/15 12:12:53 by akostian         ###   ########.fr       */
+/*   Updated: 2024/04/15 13:11:49 by akostian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,5 +87,6 @@ typedef struct s_list
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
