208f82f14f29fe93112715352ca399d9d92bcae1
Aleksieiev Kostiantyn <akostian@student.s19.b>
2024-04-17 13:40:53 +0200

    ft_strrchr fix

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,29 +6,36 @@
 /*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 14:41:47 by akostian          #+#    #+#             */
-/*   Updated: 2024/04/11 16:41:24 by akostian         ###   ########.fr       */
+/*   Updated: 2024/04/16 15:59:03 by akostian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*last_occ;
 
-	last_occ = (void *)0;
-	while (*s)
+	last_occ = NULL;
+	while (1)
 	{
 		if (*s == c)
 			last_occ = (char *)s;
+		if (!*s)
+			break ;
 		s++;
 	}
 	return (last_occ);
 }
 /* 
 #include <stdio.h>
+#include <string.h>
+
 int	main(void)
 {
-	char	testcase1[22] = "jshfisKeiufhsieufhus";
+	const char	testcase1[22] = "jshfisKeiufhsieufhus";
 
-	printf("%s", ft_strrchr(testcase1, 'i'));
+	printf("'%s'\n", ft_strrchr(testcase1, '\0'));
+	printf("'%s'\n", strrchr(testcase1, '\0'));
 }
  */
