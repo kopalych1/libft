c9e5ff3fa0ef32bcd188ff8a7785f18cbf12f162
Aleksieiev Kostiantyn <akostian@student.s19.b>
2024-04-17 13:43:52 +0200

    ft_strtrim fix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 20:32:11 by akostian          #+#    #+#             */
-/*   Updated: 2024/04/11 20:32:11 by akostian         ###   ########.fr       */
+/*   Updated: 2024/04/17 11:54:29 by akostian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 
-	if (!s1 || !set)
-		return (0);
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
+	if (!*s1)
+		return (ft_substr(s1, 0, 1));
 	i = ft_strlen(s1) - 1;
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i--;
@@ -30,5 +34,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 int	main(void)
 {
-	printf("%s", ft_strtrim("ABCisjeofijsoeifjis>>>>", "AB>"));
+	printf("'%s'\n", ft_strtrim("AAAAAAAA", "A"));
+	printf("'%s'\n", ft_strtrim("AAAAA.sssssaadawsssa.AAA", "A"));
+	printf("'%s'\n", ft_strtrim("AAA,AadddsqwjsqweqwwwaaaAAABB;BBCCCCA", "ABC"));
 } */
